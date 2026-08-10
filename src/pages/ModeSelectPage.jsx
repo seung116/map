8733233602105977f4933c6984e99bf2cf38981a
김승혁 <sanaa@MacBook-Air-2.local>
2026-08-10 18:14:47 +0900

    Update main screen admin link

--- src/pages/ModeSelectPage.jsx
+++ src/pages/ModeSelectPage.jsx
@@ -31,7 +31,6 @@ export default function ModeSelectPage() {
         </div>
 
         <div className="mode-page-links">
-          <Link to="/mypage">마이페이지</Link>
           {auth?.isAdmin && <Link className="mode-admin-link" to="/admin">관리자페이지</Link>}
         </div>
       </main>
